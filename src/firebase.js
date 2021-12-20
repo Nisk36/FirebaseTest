10ec7afebed297a5d566f5d27ffb0ff1fc7b4d48
Nisk <nisknishimura@gmail.com>
2021-12-21 00:36:06 +0900

    first Commit

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-storage.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBtJ2EN7_JznLs03fYngKzl0nN6F9-5G9I",
+authDomain: "vuetest-103b3.firebaseapp.com",
+projectId: "vuetest-103b3",
+storageBucket: "vuetest-103b3.appspot.com",
+messagingSenderId: "640887077281",
+appId: "1:640887077281:web:a87322542e2246c1f2f3e0"
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
+
+
+
